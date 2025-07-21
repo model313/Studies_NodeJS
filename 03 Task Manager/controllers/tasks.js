ef1759ed72e87282aeca7bed012a48975b6b44af
model313 <dontgoap@gmail.com>
2025-07-21 18:32:49 +0900

    feat: add CRUD functions for tasks

--- 03 Task Manager/controllers/tasks.js
+++ 03 Task Manager/controllers/tasks.js
@@ -3,8 +3,13 @@
 const Task = require('../models/task')
 
 // Gets all active tasks
-const getAllTasks = (req, res) => {
-    res.send('All Tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 // Create task
@@ -18,19 +23,59 @@ const createTask = async (req, res) => {
 }
 
 // Get single task
-const getTask = (req, res) => {
-    res.json({id: req.params.id})
-}
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        
+        // If task does not exist (does not include Cast Error)
+        // CastError: an error that Mongoose throws if syntax differs in length
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
 
-// Update task
-const updateTask = (req, res) => {
-    res.send('Update Task')
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 // Delete task
-const deleteTask = (req, res) => {
-    res.send('Delete Task')
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+        
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
+
+// Update task
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if(!task){
+            return res.status(404).json({msg: `No task with id: ${taskID}`})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+}
+
 
 // app.js > routes > controllers
 module.exports = {
