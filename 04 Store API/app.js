279792a5f4b330ba27ec1f0f325176018330f87a
model313 <dontgoap@gmail.com>
2025-07-23 19:00:30 +0900

    feat: express server basic setup

--- 04 Store API/app.js
+++ 04 Store API/app.js
@@ -13,5 +13,21 @@ app.use(express.json())
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('<h1>Store API</h1><a href="/api/v1/products"')
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
 })
+
+// Products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 8080
+const start = async () => {
+    try {
+        // ConnectDB
+        app.listen(port, console.log(`Server is listening to port: ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
