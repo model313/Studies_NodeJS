e45877a646eaac23303906e26258a75cf7794378
model313 <dontgoap@gmail.com>
2025-07-23 18:56:16 +0900

    feat: add custom error handling

--- 04 Store API/app.js
+++ 04 Store API/app.js
@@ -1,1 +1,17 @@
-console.log('04 Store API')
+require('dotenv').config()
+// Async errors
+
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/notFound')
+const errorMiddleware = require('./middleware/errorHandler')
+
+// Middleware
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products"')
+})
