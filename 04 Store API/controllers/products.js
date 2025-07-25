25d6524f7567295220c4caefa8fc32b5a1452632
model313 <dontgoap@gmail.com>
2025-07-25 18:25:25 +0900

    feat: add sort filter params

--- 04 Store API/controllers/products.js
+++ 04 Store API/controllers/products.js
@@ -3,7 +3,7 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort} = req.query
     const queryObject = {}
 
     if (featured) {
@@ -21,7 +21,14 @@ const getAllProducts = async (req, res) => {
     // Just putting in the req.query in find() works
     // BUT we can not handle unrelated query params
     // const products = await Product.find(req.query)
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({count: products.length, products})
 }
 
