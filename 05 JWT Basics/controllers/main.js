d8c573fcf88d252646fe4dfe3e6313ef6cfe1c11
model313 <dontgoap@gmail.com>
2025-07-28 17:08:30 +0900

    feat: add basic routes

---
+++ 05 JWT Basics/controllers/main.js
@@ -0,0 +1,16 @@
+// JWT Controller
+
+const login = async (req, res) => {
+  res.send("Fake Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res
+    .status(200)
+    .json({ msg: "Hello", secret: `Lucky number is: ${luckyNumber}`});
+};
+
+module.exports = {
+  login, dashboard
+}
---
+++ 05 JWT Basics/routes/main.js
@@ -0,0 +1,11 @@
+// JWT Routes
+
+const express = require('express')
+const router = express.Router()
+
+const {login, dashboard} = require('../controllers/main')
+
+router.route('/dashboard').get(dashboard)
+router.route('/login').get(login)
+
+module.exports = router
