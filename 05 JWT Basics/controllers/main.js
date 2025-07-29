f8be7b07c5c49500214700a09e4228f7b1d82cde
model313 <dontgoap@gmail.com>
2025-07-29 17:28:11 +0900

    feat: add JWT validation

--- 05 JWT Basics/controllers/main.js
+++ 05 JWT Basics/controllers/main.js
@@ -22,6 +22,8 @@ const login = async (req, res) => {
   // User ID placeholder (usually called from DB)
   const id = new Date().getDate()
 
+
+  // Create JWT
   // Light payload is best for performance
   // Secret key should be long, complex string value
   const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
@@ -30,11 +32,47 @@ const login = async (req, res) => {
   res.status(200).json({msg: 'User Created', token})
 };
 
+/**
+ * In this project, the front-end will recieve the signed token 
+ * and store it in local storage. Note that there are several other ways 
+ * to store a JWT, all with pros and cons. 
+ * 
+ * The front-end will then add a request header whenever it needs to:
+ * Authorization: Bearer ${token}
+ * 
+ * Remember to add this header in Postman when testing.
+ */
+
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({ msg: "Hello", secret: `Lucky number is: ${luckyNumber}`});
+  // Should log all headers, including authorization
+  // console.log(req.headers)
+
+  // Get header
+  const authHeader = req.headers.authorization
+  
+  // Empty/invalid header error
+  if(!authHeader || !authHeader.startsWith('Bearer ')) {
+    // Should be 401 error with invalid credentials msg
+    throw new CustomAPIError('No token provided', 400)
+  }
+
+  // Parse authorization header
+  const token = authHeader.split(' ')[1]
+  
+  // Verify token
+  try {
+    // Parse token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    // console.log(decoded)
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+  
+    res
+      .status(200)
+      .json({ msg: `Hello, ${decoded.username}`, secret: `Lucky number is: ${luckyNumber}`});
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401)
+  }
 };
 
 module.exports = {
