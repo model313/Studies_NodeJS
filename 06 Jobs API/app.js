106e866576f465aed17c8c8215545935f4e8ef07
model313 <dontgoap@gmail.com>
2025-08-01 19:22:11 +0900

    feat: add basic routes

--- 06 Jobs API/app.js
+++ 06 Jobs API/app.js
@@ -3,17 +3,23 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// Connect DB
+
+// Routers
+const authRouter = require('./routes/auth')
+const jobRouter = require('./routes/jobs')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
 
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+// Extra Packages
+
+// Routes
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 06 Jobs API/controllers/jobs.js
@@ -0,0 +1,28 @@
+const getAllJobs = async (req, res) => {
+  res.send('get all jobs')
+}
+
+const getJob = async (req, res) => {
+  res.send('get job')
+}
+
+const createJob = async (req, res) => {
+  res.send('create')
+}
+
+const updateJob = async (req, res) => {
+  res.send('update')
+}
+
+const deleteJob = async (req, res) => {
+  res.send('delete')
+}
+
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob
+}
