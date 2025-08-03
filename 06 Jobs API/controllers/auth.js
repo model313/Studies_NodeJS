9d365eaf78299fb69acf43a7b0f2fa8d19b426ba
model313 <dontgoap@gmail.com>
2025-08-03 16:49:38 +0900

    feat: add create user

--- 06 Jobs API/controllers/auth.js
+++ 06 Jobs API/controllers/auth.js
@@ -1,5 +1,20 @@
+// Auth Controller (Users)
+
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+
 const register = async (req, res) => {
-  res.send('register')
+  // This is already covered with mongoose validators in User.js
+  // const {name, email, password} = req.body
+  // if(!name || !email || !password){
+  //   throw new BadRequestError('Please provide name, email and password')
+  // }
+
+
+
+  const user = await User.create({...req.body})
+  res.status(StatusCodes.CREATED).json({user})
 }
 
 const login = async (req, res) => {
