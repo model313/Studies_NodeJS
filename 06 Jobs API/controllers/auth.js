83a40ceab34da00e307478fd32316e0a0fef79ae
model313 <dontgoap@gmail.com>
2025-08-04 17:09:33 +0900

    feat: add password hashing for user

--- 06 Jobs API/controllers/auth.js
+++ 06 Jobs API/controllers/auth.js
@@ -3,17 +3,35 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError} = require('../errors')
+const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
+  // Check if data exists
   // This is already covered with mongoose validators in User.js
   // const {name, email, password} = req.body
   // if(!name || !email || !password){
   //   throw new BadRequestError('Please provide name, email and password')
   // }
 
+  // Extract elements from body
+  const {name, email, password} = req.body
 
+  // Hash password
+  const salt = await bcrypt.genSalt(10)   // Generates random bytes (default is 10)
+  const hashedPassword = await bcrypt.hash(password, salt)
 
-  const user = await User.create({...req.body})
+  // Create temporary object
+  const tempUser = {name, email, password:hashedPassword}
+
+  // Create user
+  /**
+   * ... = Spread Operator
+   * Unpacks all key-value pairs from the tempUser object and passes them as 
+   * individual properties to User.create()
+   */
+  const user = await User.create({...tempUser})
+
+  // Respond success
   res.status(StatusCodes.CREATED).json({user})
 }
 
