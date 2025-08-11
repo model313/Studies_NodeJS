4380bff8e66851b24ebd846b4f90c461c65fc1b8
model313 <dontgoap@gmail.com>
2025-08-11 18:12:29 +0900

    feat: apply authentication to job routes

--- 06 Jobs API/app.js
+++ 06 Jobs API/app.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect DB
 const connectDB = require('./db/connect')
 
+// JWT Authentication
+const authenticateUser = require('./middleware/authentication')
+
 // Routers
 const authRouter = require('./routes/auth')
 const jobRouter = require('./routes/jobs')
@@ -20,7 +23,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/jobs', authenticateUser, jobRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- 06 Jobs API/controllers/jobs.js
+++ 06 Jobs API/controllers/jobs.js
@@ -9,7 +9,7 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-  res.send('create')
+  res.json(req.user)
 }
 
 const updateJob = async (req, res) => {
