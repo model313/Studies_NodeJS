0759b0e547bbad4a91d5aa177b520e5725772c1e
model313 <dontgoap@gmail.com>
2025-08-13 13:15:02 +0900

    feat: add get all jobs

--- 06 Jobs API/controllers/jobs.js
+++ 06 Jobs API/controllers/jobs.js
@@ -1,38 +1,43 @@
 // Jobs Controller
 
-const Job = require('../models/Job')  // Creates collection when running (even w/o invoking)
-const {StatusCodes} = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require('../errors')
+const Job = require("../models/Job") // Creates collection when running (even w/o invoking)
+const { StatusCodes, OK } = require("http-status-codes")
+const { BadRequestError, NotFoundError } = require("../errors")
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs')
+  // Get all jobs associated with user
+
+  const jobs = await Job
+  .find({ createdBy: req.user.userId })
+  .sort("createdAt")
+
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length})
 }
 
 const getJob = async (req, res) => {
-  res.send('get job')
+  res.send("get job")
 }
 
 const createJob = async (req, res) => {
   // Add user data to req.body for createdBy data field in Jobs schema
   req.body.createdBy = req.user.userId
   const job = await Job.create(req.body)
-  
+
   res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
-  res.send('update')
+  res.send("update")
 }
 
 const deleteJob = async (req, res) => {
-  res.send('delete')
+  res.send("delete")
 }
-
 
 module.exports = {
   getAllJobs,
   getJob,
   createJob,
   updateJob,
-  deleteJob
+  deleteJob,
 }
